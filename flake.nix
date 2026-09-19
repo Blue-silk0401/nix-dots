33b0dbb73beebbd1a2638cde91ed2a7e2498c2fe
Blue-silk0401 <Bluish_silk@proton.me>
2026-09-19 21:30:35 +0300

    Configure nvf

--- flake.nix
+++ flake.nix
@@ -1,13 +1,38 @@
 {
   description = "A very basic flake";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
-  };
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+		home-manager = {
+			url = "github:nix-community/home-manager/release-26.05";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+		nvf.url = "github:notashelf/nvf";
+	};
+  	
+	outputs = { self, nixpkgs, home-manager,nvf, ... }: 
+		let
+			system = "x86_64-linux";
+		in {
 
-  outputs = { self, nixpkgs }: {
-      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-	  modules = [ ./configuration.nix ];
-    };
-  };
+		packages.${system}.default = 
+		(nvf.lib.neovimConfiguration {
+			pkgs = nixpkgs.legacyPackages.${system};
+			modules = [ ./nvf-configuration.nix ];
+		}).neovim;
+
+		nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+			inherit system;
+			modules = [ 
+			./configuration.nix 
+			nvf.nixosModules.default
+			];
+    			};
+
+		homeConfiguration.amper = home-manager.lib.homeManagerConfiguration {
+			pkgs = nixpkgs.legacyPackages.${system};
+			modules = [ ./home.nix ];
+
+  		};
+	};
 }
